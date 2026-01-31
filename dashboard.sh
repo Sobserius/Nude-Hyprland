849009615b7c621b714c2ffca6511d69d5fe98e0
Sobserius <151635095+Sobserius@users.noreply.github.com>
2026-01-31 22:53:25 +1000

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# WOID - Worst Operatable Irregular Dash
-#hyprctl dispatch focuswindow "class:dash-box"
+
 hyprctl --batch "keyword cursor:no_warps true; dispatch focuswindow class:dash-box; keyword cursor:no_warps false"
 
 
@@ -11,7 +10,7 @@ done
 
 
 cleanup() {
-    # RESTORE SYSTEM STATE
+
     hyprctl keyword device:all:enabled true > /dev/null 2>&1
     hyprctl --batch "keyword cursor:no_hardware_cursors false; \
 #                     keyword cursor:inactive_timeout 0; \
@@ -26,20 +25,11 @@ cleanup() {
 trap cleanup SIGINT SIGTSTP EXIT SIGUSR1
 
 
-#  hyprctl dispatch focuswindow class:dash-box >/dev/null 2>&1
-
 get_sys()  {
 echo "$USER@$HOSTNAME"
 }
 
-#get_stats() {
-#    local time=$(date +'%H:%M')
-#    local lang=$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap' | cut -c1-2 | head -n 1)
-#    local vol_data=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-#    if [[ "$vol_data" == *"[MUTED]"* ]]; then local vol="MUTE"; else local vol=$(echo "$vol_data" | awk '{print int($2*100)"%"}'); fi
-#    local bat=$(cat /sys/class/power_supply/BAT*/capacity 2>/dev/null | head -n 1 || echo "0")
-#    printf " Clk: %s  |  Vol: %s  |  Lan: %s  |  Bat: %s%% " "$time" "$vol" "${lang:-EN^^}" "$bat"
-#}
+
 
 tput civis 
 clear
@@ -48,14 +38,10 @@ while true; do
 
 
 
-    # Main Loop Focus Guard (Silent)
-#    hyprctl dispatch focuswindow class:dash-box >/dev/null 2>&1
     echo -ne "\033[H"
     echo -e  "\n\n\n\n   			  $(get_sys) \n\n\n\n\n"
-#    echo -e "\n   $(get_stats)"
     echo -e "  +------------------------------------------------------------+"
     echo -e "   1. LAUNCH APPS"
-#    echo -e "   2. KILL PROCESSES"
     echo -e "   2. CHECK UPDATES"
     echo -e "   3. SCREENSHOT"
     echo -e "   4. POWER MENU"
@@ -84,47 +70,34 @@ fi
    fi
    tput civis ;;
 
-#2) tput cnorm
-#   USER_PROCS=$(ps -u $USER -o comm= | sort -u | grep -vE "<-server|Utility|Web|WebExtensions|hyprland|Hyprland|Xwayland|steamwebhelper|steam-runtime-l|xdg-|dbus-|systemd|waybar|sway|pipewire|wireplumber|sh$|bash$|fzf|bwrap|cat|swww|pdm|dconf|at-spi|gvfsd|flatpak|crashhelper|forkserver|isolated|zypak|sort|Socket|sd-pam|RDD|ps$|Privileged|p11-kit|kitten|inotifywait|polkit|dunst|grep")
-#   SELECTED=$(echo "$USER_PROCS" | fzf --reverse --prompt="KILL > ")
 
-#   if [ -n "$SELECTED" ]; then
-#           pkill -9 "$SELECTED"
-       
-#       cleanup
-#   fi
-#   tput civis ;;
 
         2) (nohup hyprctl dispatch exec "[workspace f+0] kitty sudo pacman -Syu" >/dev/null 2>&1 &); sleep 0.1; cleanup ;;
         3) (nohup hyprctl dispatch exec "[workspace f+0] sleep 0.3 && /home/$USER/.config/dash/screenshot.sh" >/dev/null 2>&1 &); sleep 0.1; cleanup ;;        
         4) echo -e "\n  [ 1.SHUTDOWN  2.REBOOT  3.LOGOUT  4.LOCK  0.BACK ]"
            read -rsn1 p_input
-           case "$p_input" in 1) systemctl poweroff ;; 2) systemctl reboot ;; 3) loginctl terminate-user "$USER" ;; 4)  nohup hyprlock  & sleep 0.1 & exit   ;; *) clear ;;  esac ;;
-#        5) tput cnorm  
-#           RAW_CMD2=$($HOME/.config/themes/color.sh)
-#   if [ -n "$RAW_CMD2" ]; then
-#       clear 
-#       hyprctl dispatch exec -- "$RAW_CMD2" >/dev/null 2>&1
-#       cleanup
-#       exit 0 
-#   fi
-#   tput civis ;;
-#
+           case "$p_input" in 1) systemctl poweroff ;; 2) systemctl reboot ;; 3) loginctl terminate-user "$USER" ;; 4) nohup hyprlock & cleanup   ;; *) clear ;;  esac ;;
 
-#	5) tput cnorm; $HOME/.config/themes/color.sh;  clear ;;
-        5) tput cnorm
-	   bash "$HOME/.config/themes/tools/picker.sh"
-           STATUS=$?
-
-           if [ $STATUS -eq 0 ]; then
-               cleanup
-           else
-               clear
-               tput civis
-           fi ;;
-
-#        6) tput cnorm; rm ~/.config/kitty/dark-theme.auto.conf; rm ~/.config/kitty/light-theme.auto.conf; rm ~/.config/kitty/no-preference-theme.auto.conf; sleep 3; cleanup ;;
-#        7) tput cnorm; hyprctl activewindow -j | jq  '.title, .class';;
+        5) echo -e "\n  [ 1.COLORS 2.WAYBARS 0.BACK ]"
+           read -rsn1 p_input
+           case "$p_input" in 
+               1) tput cnorm
+                  bash "$HOME/.config/themes/tools/picker.sh"
+                  STATUS=$?
+                  if [ $STATUS -eq 0 ]; then
+                      cleanup
+                  else
+                      clear
+                      tput civis
+                  fi ;;
+               2) tput cnorm
+                  bash "$HOME/.config/waybar/barmanager.sh"
+                  clear
+                  tput civis ;;
+               0) clear ;;
+               *) clear ;;
+           esac ;;
         0|q) cleanup ;;
     esac
 done
+
